2bae834097f8168865795f70ec4f1b980878540c
K-ost <golubkovru@gmail.com>
2025-09-28 11:21:11 +0300

    linter and actions settings

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
+import pluginObject from "eslint-plugin-unused-imports";
 
 export default defineConfig([
   globalIgnores(["dist"]),
@@ -15,6 +16,9 @@ export default defineConfig([
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      "unused-imports": pluginObject,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -22,6 +26,17 @@ export default defineConfig([
     rules: {
       "no-console": "warn",
       "no-alert": "off",
+      "unused-imports/no-unused-imports": "warn",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: true,
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+        },
+      ],
     },
   },
 ]);
