a402c293c07966f2da42bb0d0f153d8419b13356
K-ost <golubkovru@gmail.com>
2025-10-01 09:25:08 +0300

    utils tests

--- src/tests/testUtilities.tsx
+++ src/tests/testUtilities.tsx
@@ -1,5 +1,6 @@
 import { HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { Product } from "../types";
 
 const mockQueryClient = new QueryClient();
 export const Wrapper = ({ children }: { children: React.ReactNode }) => {
@@ -9,3 +10,28 @@ export const Wrapper = ({ children }: { children: React.ReactNode }) => {
     </HashRouter>
   );
 };
+
+export class ProductFactory {
+  #categories: string[] = ["clothes", "other"];
+  #names: string[] = ["Shirt", "Pants", "Jacket", "Socks", "Hat"];
+  createProduct(id: number, title: string, category: string): Product {
+    return {
+      category,
+      description: "",
+      id,
+      image: "mock.image",
+      price: 100 + id * 10,
+      rating: {
+        count: 1,
+        rate: 5,
+      },
+      title,
+    };
+  }
+  createProductsList(): Product[] {
+    return Array.from({ length: 5 }, (__, k) => {
+      const cat = k < 3 ? this.#categories[0] : this.#categories[1];
+      return this.createProduct(k, this.#names[k], cat);
+    });
+  }
+}
---
+++ src/tests/utils.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from "vitest";
+import { FilterController } from "../utils/FilterController";
+import { ProductFactory } from "./testUtilities";
+
+const filterController = new FilterController();
+const productFactory = new ProductFactory();
+const mockedList = productFactory.createProductsList();
+
+describe("Utils", () => {
+  describe("Filter Controller", () => {
+    it("Search", () => {
+      const output = filterController.filterProducts(mockedList, "Shirt", "clothes");
+      expect(output).toHaveLength(1);
+    });
+    it("Select", () => {
+      const output = filterController.filterProducts(mockedList, "", "clothes");
+      expect(output).toHaveLength(3);
+    });
+    it("Search & Select", () => {
+      const output = filterController.filterProducts(mockedList, "Hat", "other");
+      expect(output).toHaveLength(1);
+    });
+    it("Get Select Options", () => {
+      const output = filterController.getOptions(mockedList);
+      expect(output).toStrictEqual(["all", "clothes", "other"]);
+      expect(output).toHaveLength(3);
+    });
+  });
+});
