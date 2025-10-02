526218217fafd00181389e79109082081921054e
K-ost <golubkovru@gmail.com>
2025-10-02 20:17:49 +0300

    lazy inner pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import type { JSX } from "react";
+import { lazy, Suspense, type JSX } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Navmenu from "./components/Navmenu";
-import Bookmarks from "./pages/Bookmarks";
-import ProductPage from "./pages/Product";
+
+const Bookmarks = lazy(() => import("./pages/Bookmarks"));
+const ProductPage = lazy(() => import("./pages/Product"));
 
 function App(): JSX.Element {
   return (
     <div className="container m-auto px-4 py-4">
       <Navmenu />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/bookmarks" element={<Bookmarks />} />
-        <Route path="/:id" element={<ProductPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bookmarks" element={<Bookmarks />} />
+          <Route path="/:id" element={<ProductPage />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
